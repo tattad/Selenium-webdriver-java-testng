c3fa21d2688d76a0e72ddc070e064ced9416868a
dat.tran <dattran0808@gmail.com>
2024-03-26 17:17:36 +0700

    Met qua toi dia lam tiep

---
+++ selenium/javaTester/Topic_01_Data_Type.java
@@ -0,0 +1,26 @@
+package javaTester;
+
+public class Topic_01_Data_Type {
+    //Kieu du lieu trong Java - 2 nhom
+
+    //Nhóm 1: Kiểu dữ liệu nguyên thủy (Primitive Type)
+    //8 loại
+    //So nguyen: byte - short - int - long
+    //Khong co phan thap phan: nhan vien trong 1 cty/ hoc sinh trong 1 lop
+
+    //So thuc: float - double
+    //Co phan thap phan: diem so/ luong/...
+
+    //Ki tu: char
+    //Logic: boolean
+
+    //Nhom 2: Kieu du lieu tham chieu (Reference Type)
+
+
+    //Khai bao 1 bien de luu tru 1 loai du lieu nao do
+    //Access Modifier: Pham vi truy cap (public/ private/ protected/ default)
+    //Kieu du lieu
+    //Ten bien
+    //Gia tri cua bien - gan vao voi phep =
+    public int studentNumber = 200;
+}
--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -24,16 +24,74 @@ public class Topic_02_Selenium_Locator {
 
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://demo.nopcommerce.com/register");
         driver.manage().window().maximize();
     }
 
+//    @Test
+//    public void TC_01(){
+//        //Selenium 1.x, 2.x, 3.x co 8 loai locator
+//        //Selenium locator = HTLM attribute
+//        //Id/ Class/ Name =  trùng với 3 attribute cua HTLM
+//        //Link text/ Partial LinkText: HTML link (thẻ a)
+//        //Tag name: HTML tag name
+//        //CSS/ Path
+//
+//        //Selenium version 4.x
+//        //Class - Relative Locator
+//        //above/ below/ near/ leftOf/ rightOf
+//
+//        //UI Testing
+//        //FUI: Functional UI
+//
+//        //GUI: Graphic UI - Visualize Testing
+//        //Font/ Size/ Color/ Position/ Location/ Resolution/ Responsive...
+//    }
+
+    //TestNG: Order testcase following Alphabet (0-9, A-Z)
+    //Firstname textbox - HTML code
+    //HTML Element: <tagname attribute_name_1='attribute_value' attribute_name_2='attribute_value'...>
+    /*
+     *<input type="text" data-val="true" data-val-required="First name is required." id="FirstName" name="FirstName">
+     */
+
     @Test
-    public void TC_01(){
+    public void TC_01_ID(){
+//        driver.findElement(By.);
+    }
+
+    @Test
+    public void TC_02_Class(){
 
     }
 
     @Test
-    public void TC_02(){
+    public void TC_03_Name(){
+
+    }
+
+    @Test
+    public void TC_04_Tagname(){
+
+    }
+
+    @Test
+    public void TC_05_LinkText(){
+
+    }
+
+    @Test
+    public void TC_06_Partial_LinkText(){
+
+    }
+
+    @Test
+    public void TC_07_Css(){
+
+    }
+
+    @Test
+    public void TC_08_XPath(){
 
     }
 
