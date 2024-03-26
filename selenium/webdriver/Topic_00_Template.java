a3c97c006e4ab412d8618526b24f28ff98d8e73f
dat.tran <dattran0808@gmail.com>
2024-03-26 15:40:30 +0700

    Create template & add new libraries

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -1,22 +1,20 @@
 package webdriver;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_Environment {
+import java.util.concurrent.TimeUnit;
+
+public class Topic_00_Template {
+
     WebDriver driver;
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
 
     @BeforeClass
-    public void beforeClass() {
+    public void beforeClass(){
         if (osName.contains("Windows")) {
             System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
         } else {
@@ -36,25 +34,5 @@ public class Topic_01_Check_Environment {
     @Test
     public void TC_02(){
 
-    }
-
-    @Test
-    public void TC_01_Url() {
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
-    }
-
-    @Test
-    public void TC_02_Logo() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
-    }
-
-    @Test
-    public void TC_03_Form() {
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
-    }
-
-    @AfterClass
-    public void afterClass() {
-        driver.quit();
     }
 }
