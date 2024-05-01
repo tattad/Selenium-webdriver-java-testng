129fa56a86c3f6d3907702ce64866b31051fb953
taddat <dattran0808@gmail.com>
2024-05-01 21:03:18 +0700

    User Interactions P1

---
+++ selenium/webdriver/Topic_14_Actions.java
@@ -0,0 +1,62 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_14_Actions {
+
+    WebDriver driver;
+
+    Actions actions;
+
+    String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        actions = new Actions(driver);
+
+        setFullName("Automation testing");
+    }
+
+    @Test
+    public void TC_01_Hover() {
+        driver.get("https://automationfc.github.io/jquery-tooltip/");
+    }
+
+    @Test
+    public void TC_02_() {
+    }
+
+    @AfterClass
+    public void afterClass() {
+//        driver.quit();
+    }
+
+    public void sleepInSecond(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
