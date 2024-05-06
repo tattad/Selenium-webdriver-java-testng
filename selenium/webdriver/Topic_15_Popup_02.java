d18246b94877edf3989159f8fa5cc93700dd6495
dat.tran <dattran0808@gmail.com>
2024-05-06 17:32:41 +0700

    Handle Popup/Dialog P2 + Shadow DOM

--- selenium/webdriver/Topic_15_Popup_02.java
+++ selenium/webdriver/Topic_15_Popup_02.java
@@ -1,6 +1,7 @@
 package webdriver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,6 +11,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Topic_15_Popup_02 {
 
@@ -25,43 +27,66 @@ public class Topic_15_Popup_02 {
     @Test
     public void TC_01_Random_Popup_In_DOM_01() {
         driver.get("https://www.javacodegeeks.com/");
-        sleepInSecond(10);
+        sleepInSecond(20);
 
-        WebElement randomPopup = driver.findElement(By.xpath("//div[@class='lepopup-popup-container']/div[contains(@class,'lepopup-form') and not(contains(@style,'display:none'))]"));
+        List<WebElement> newsletterPopup = driver.findElements(By.xpath("//div[@class='lepopup-popup-container']/div[contains(@class,'lepopup-form') and not(contains(@style,'display:none'))]"));
 
-        if (randomPopup.isDisplayed()) {
+        if (newsletterPopup.size() > 0 && newsletterPopup.get(0).isDisplayed()) {
+            System.out.println("Co hien thi");
             driver.findElement(By.xpath("//div[@class='lepopup-popup-container']/div[contains(@class,'lepopup-form') and not(contains(@style,'display:none'))]//a[text()='No Thanks!']")).click();
             sleepInSecond(2);
-            driver.findElement(By.id("search-input")).sendKeys("Agile Testing Explained");
-            driver.findElement(By.id("search-submit")).click();
-            sleepInSecond(5);
-
-            Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Agile Testing Explained']")).isDisplayed());
         } else {
-            driver.findElement(By.id("search-input")).sendKeys("Agile Testing Explained");
-            driver.findElement(By.id("search-submit")).click();
-            sleepInSecond(5);
-
-            Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Agile Testing Explained']")).isDisplayed());
+            System.out.println("Khong hien thi");
         }
+
+        driver.findElement(By.id("search-input")).sendKeys("Agile Testing Explained");
+        driver.findElement(By.id("search-submit")).click();
+        sleepInSecond(5);
+
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Agile Testing Explained']")).isDisplayed());
     }
 
     @Test
     public void TC_02_Random_Popup_In_DOM_02() {
         driver.get("https://vnk.edu.vn/");
+//        sleepInSecond(30);
 
-        WebElement randomPopup = driver.findElement(By.xpath("//div[@id='tve-p-scroller']/article"));
+//        WebElement marketingPopup = driver.findElement(By.xpath("//div[@class='tve-leads-conversion-object']"));
+
+        findElement(By.xpath("//button[text()='Danh sách khóa học']")).click();
+        sleepInSecond(2);
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='title-content']/h1")).getText(), "Lịch Khai Giảng Tháng 05");
     }
 
     @Test
     public void TC_03_Random_Popup_Not_In_DOM() {
         driver.get("https://dehieu.vn/");
 
-        WebElement randomPopup = driver.findElement(By.xpath(""));
+        List<WebElement> registerFormPopup = driver.findElements(By.xpath("//div[contains(@class,'modal-content')]"));
+
+        if (registerFormPopup.size() > 0 && registerFormPopup.get(0).isDisplayed()) {
+            int heightBrowser = driver.manage().window().getSize().getHeight();
+            if (heightBrowser < 1920) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click;", driver.findElement(By.xpath("//button[@class='close']")));
+            } else {
+                driver.findElement(By.xpath("//button[@class='close']")).click();
+            }
+
+            sleepInSecond(2);
+            System.out.println("Co hien thi");
+        } else {
+            System.out.println("Khong hien thi");
+        }
     }
 
-    @Test
-    public void TC_04_Shadow_DOM() {
+    public WebElement findElement(By locator) {
+        if (driver.findElement(By.xpath("//div[@class='tve-leads-conversion-object']")).isDisplayed()) {
+            driver.findElement(By.xpath("//div[contains(@class,'tve_ea_thrive_leads_form_close')]")).click();
+            sleepInSecond(2);
+            System.out.println("Co hien thi");
+        }
+        return driver.findElement(locator);
     }
 
     @AfterClass
---
+++ selenium/webdriver/Topic_16_Shadow_DOM.java
@@ -0,0 +1,88 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Topic_16_Shadow_DOM {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Shadow_DOM() {//Chưa support cho xpath, cần dùng css
+        driver.get("https://automationfc.github.io/shadow-dom/");
+        sleepInSecond(2);
+
+        //Đi theo đúng cấu trúc của HTML/DOM
+        //shadowRootElement = đại diện cho cái shadow DOM 1 bên trong
+        WebElement shadowHost = driver.findElement(By.cssSelector("div#shadow_host"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+
+        String someText = shadowRoot.findElement(By.cssSelector("span#shadow_content>span")).getText();
+        System.out.println(someText);
+        Assert.assertEquals(someText, "some text");
+
+        WebElement checboxShadow = shadowRoot.findElement(By.cssSelector("input[type='checkbox']"));
+        Assert.assertFalse(checboxShadow.isSelected());
+
+        List<WebElement> allInput = shadowRoot.findElements(By.cssSelector("input"));
+        System.out.println(allInput.size());
+
+        //nestedShadowHostElement = đại diện cho cái nested shadow DOM 2 (nằm trong shadow DOM 1)
+        WebElement nestedShadowHost = shadowRoot.findElement(By.cssSelector("div#nested_shadow_host"));
+        SearchContext nestedShadowRoot = nestedShadowHost.getShadowRoot();
+
+        String nestText = nestedShadowRoot.findElement(By.cssSelector("div#nested_shadow_content>div")).getText();
+        Assert.assertEquals(nestText, "nested text");
+    }
+
+    @Test
+    public void TC_02_Shadow_DOM_Shopee() {
+        driver.get("https://shopee.vn/");
+        sleepInSecond(5);
+
+        WebElement shadowHost = driver.findElement(By.cssSelector("shopee-banner-simple"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+
+        //Verify pop hiển thị
+        if (shadowRoot.findElement(By.cssSelector("div.home.home-popup__content")).isDisplayed()) {
+            shadowRoot.findElement(By.cssSelector("div.home-popup__close-button")).click();
+            sleepInSecond(2);
+        }
+
+        driver.findElement(By.xpath("//input[contains(@class,'shopee-searchbar')]")).sendKeys("Samsung S25 Ultra");
+        sleepInSecond(2);
+
+        driver.findElement(By.xpath("//button[contains(@class,'shopee-searchbar__search-button')]")).click();
+    }
+
+    @AfterClass
+    public void afterClass() {
+//        driver.quit();
+    }
+
+    public void sleepInSecond(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
